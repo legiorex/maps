7637ab82677bcd039b7b457d7020a838b5e06619
legiorex <37956597+legiorex@users.noreply.github.com>
2018-09-05 19:31:17 +1000

    Сделал добавление в кластер и отслеживание клика на маркере

--- source/js/script.js
+++ source/js/script.js
@@ -6,23 +6,20 @@ const userName = document.querySelector('.name-user');
 const place = document.querySelector('.place');
 const review = document.querySelector('.review');
 const [date, time] = new Date().toLocaleString().split(', ');
+let coords;
 
 
-let dataReview = [];
+let dataReview = {
+    type: 'FeatureCollection',
+    features: []
+};
+
+
 let storage = localStorage;
 
 
 
 
-
-
-
-
-let dataReview
-
-let listPoint = [[43.119784, 131.893635], [43.119726, 131.893611], [43.119708, 131.893674]];
-
-// center: [43.119796, 131.893628],
 
 
 
@@ -51,48 +48,27 @@ function init() {
         gridSize: 32,
         clusterDisableClickZoom: true
     });
+    // Подписываемся на событие клика по объекту.
+    objectManager.objects.events.add(['click'], function (e) {
+        // objectId – идентификатор объекта, на котором произошло событие.
+        let objectId = e.get('objectId'),
+            object = objectManager.objects.getById(objectId);
+        // Выведем информацию об объекте.
+        console.log('Тип объекта: ' + object.geometry.type);
+        console.log('Координаты объекта: ' + object.geometry.coordinates);
+    });
 
-<<<<<<< HEAD
-
-
+    // console.log(objectManager.events);
+    // objectManager.addEventListener('click', () => {
+    //     console.log('test');
+    // })
 
     // Слушаем клик на карте.
     myMap.events.add('click', function (e) {
-        const coords = e.get('coords');
-        myMap.geoObjects
-            .add(new ymaps.Placemark(coords, {
-                balloonContentHeader: 'Это новая точка'
-            }, {
-                preset: 'islands#icon',
-                iconColor: '#0095b6'
-            }))
-
-=======
-    ClusterBalloonContentItemLayout = ymaps.templateLayoutFactory.createClass([
-        '<div class=entry>',
-        '<div class=bold>$[item.place]</div>',
-        '<div>$[item.review]</div>',
-        '<div class=author>$[item.date]</div>',
-        '</div>'
-    ].join(''));
-
-
-    // for(let point of listPoint){
-    //     myMap.geoObjects
-    //         .add(new ymaps.Placemark(point, {
-    //             balloonContent: 'Это новая точка'
-    //         }, {
-    //             preset: 'islands#icon',
-    //             iconColor: '#0095b6'
-    //         }))
-    //
-    // }
->>>>>>> c205b6e0e5636dca59b375e200c75f84f1664ecd
-
+        coords = e.get('coords');
 
         getAddress(coords);
         clickMap();
-
 
     });
 
@@ -104,121 +80,105 @@ function init() {
         });
 
     }
-}
 
-function isClickForm(e) {
 
-    if(balloonPopUp.classList.contains('hide')){
+    function isClickForm(e) {
+
+        if(balloonPopUp.classList.contains('hide')){
             return false;
         }
 
-            let left = parseInt((balloonPopUp.style.left), 10);
-            let top = parseInt((balloonPopUp.style.top), 10);
-            let finishX = left + balloonPopUp.offsetWidth;
-            let finishY = top + balloonPopUp.offsetHeight;
+        let left = parseInt((balloonPopUp.style.left), 10);
+        let top = parseInt((balloonPopUp.style.top), 10);
+        let finishX = left + balloonPopUp.offsetWidth;
+        let finishY = top + balloonPopUp.offsetHeight;
 
 
-            if(left <= e.pageX && e.pageX <= finishX && e.pageY >= top && e.pageY <= finishY)
-            {
-                return true;
-            }
-            return false;
-}
+        if(left <= e.pageX && e.pageX <= finishX && e.pageY >= top && e.pageY <= finishY)
+        {
+            return true;
+        }
+        return false;
+    }
 
 
-function clickMap() {
-    mapBlock.addEventListener('click', (e) => {
+    function clickMap() {
+        mapBlock.addEventListener('click', (e) => {
 
             if(!isClickForm(e)){
                 balloonPopUp.classList.remove('hide');
                 balloonPopUp.style.left = e.pageX + 'px';
                 balloonPopUp.style.top = e.pageY + 'px';
 
-<<<<<<< HEAD
-
-                balloonPopUp.addEventListener('click', (e) => {
+                balloonPopUp.addEventListener('click', () => {
                     console.log('second click');
                 });
-
-=======
->>>>>>> c205b6e0e5636dca59b375e200c75f84f1664ecd
             }
         })
 
-}
-<<<<<<< HEAD
-function dataForm(myMap, coords) {
+    }
+    
+
+    
+    let curentId = 0;
     buttonSave.addEventListener('click', () => {
 
+    let obj = {
+        type: 'Feature',
+        id: curentId++,
+        geometry: {
+            type : 'Point',
+            coordinates: coords
+        },
+        properties : {
+            balloonContentHeader: place.value,
+                balloonContentBody: review.value,
+                balloonContentFooter: adressPoint.textContent
+        }
 
-        console.log('test');
-=======
-// function fn(coords){
-//     let placemarks = new ymaps.Placemark(coords, {
-//         balloonContent: 'Это новая точка'
-//     }, {
-//         preset: 'islands#icon',
-//         iconColor: '#0095b6'
->>>>>>> c205b6e0e5636dca59b375e200c75f84f1664ecd
+    };
+    dataReview.features.push(obj);
+
+        // Сохраняю данные в стородж
+        storage.data = JSON.stringify(dataReview);
+
+        objectManager.objects.options.set('preset', 'islands#greenDotIcon');
+        objectManager.clusters.options.set('preset', 'islands#greenClusterIcons');
+        myMap.geoObjects.add(objectManager);
+        objectManager.add(storage.data);
+
+        console.log(objectManager.objects.click);
+
+        userName.value = '';
+        place.value = '';
+        review.value = '';
+
     })
 
-    return placemarks;
+
 }
 
-buttonSave.addEventListener('click', () => {
-
-    let reviewObj = dataItem();
-    // Сохраняю данные в стородж
-    storage.data = JSON.stringify(dataReview.push(reviewObj));
-
-    // Создаем кластер
-
-    let clusterer = new ymaps.Clusterer({
-        // Используем макет балуна кластера "карусель"
-        clusterBalloonContentBodyLayout: "cluster#balloonCarouselContent",
-        // Используем собственный подмакет для отображения информации о геообъекте
-        clusterBalloonContentItemLayout: ClusterBalloonContentItemLayout,
-        // Устанавливаем ограничение на количество элементов в нижней панели
-        clusterBalloonPagerSize: 5,
-
-        // Установка внешнего вида нижней панели.
-        // Режим marker рекомендуется использовать с небольшим количеством элементов.
-        // clusterBalloonPagerType: 'marker',
-
-        clusterBalloonWidth: 220
-    });
+// function dataArray(coords) {
+//     // Создаем объект с данными
+//     let item = {
+//         coords: coords,
+//         date: date,
+//         time: time,
+//         name: userName.value,
+//         place: place.value,
+//         review: review.value,
+//         adress: adressPoint.textContent
+//     };
+//
+//     // Записываем данные в массив
+//     dataReview.push(item);
+//     console.log(dataReview);
+//     return dataReview;
+//
+// }
 
 
 
 
-
-
-
-    clusterer.add(fn());
-    myMap.geoObjects.add.add(clusterer);
-
-    userName.value = '';
-    place.value = '';
-    review.value = '';
-
-})
-
-function dataItem(coords) {
-    // Создаем объект с данными
-    let item = {
-        coords: coords,
-        date: date,
-        time: time,
-        name: userName.value,
-        place: place.value,
-        review: review.value,
-        adress: adressPoint.textContent
-    };
-
-    // Записываем данные в массив
-
-    // console.log('test' , dataReview);
-    return item;
-}
 
 
--- source/js/test.js
+++ source/js/test.js
@@ -1,2 +1,53 @@
-let a = '100px';
-console.log(+a);
+function fn(coords){
+    let placemarks = new ymaps.Placemark(coords, {
+        balloonContent: 'Это новая точка'
+    }, {
+        preset: 'islands#icon',
+        iconColor: '#0095b6'
+
+    })
+
+    return placemarks;
+}
+
+
+// Создаем кластер
+
+let clusterer = new ymaps.Clusterer({
+    // Используем макет балуна кластера "карусель"
+    clusterBalloonContentBodyLayout: "cluster#balloonCarouselContent",
+    // Используем собственный подмакет для отображения информации о геообъекте
+    clusterBalloonContentItemLayout: ClusterBalloonContentItemLayout,
+    // Устанавливаем ограничение на количество элементов в нижней панели
+    clusterBalloonPagerSize: 5,
+
+    // Установка внешнего вида нижней панели.
+    // Режим marker рекомендуется использовать с небольшим количеством элементов.
+    // clusterBalloonPagerType: 'marker',
+
+    clusterBalloonWidth: 220
+});
+
+
+
+clusterer.add(fn());
+myMap.geoObjects.add.add(clusterer);
+
+ClusterBalloonContentItemLayout = ymaps.templateLayoutFactory.createClass([
+    '<div class=entry>',
+    '<div class=bold>$[item.place]</div>',
+    '<div>$[item.review]</div>',
+    '<div class=author>$[item.date]</div>',
+    '</div>'
+].join(''));
+
+{
+    "type": "Feature",
+    "id": 0,
+    "geometry": {"type": "Point", "coordinates": [55.831903, 37.411961]},
+    "properties": {
+        "balloonContentHeader": "<font size=3><b><a target='_blank' href='https://yandex.ru'>Здесь может быть ваша ссылка</a></b></font>",
+        "balloonContentBody": "<p>Ваше имя: <input name='login'></p><p><em>Телефон в формате 2xxx-xxx:</em>  <input></p><p><input type='submit' value='Отправить'></p>",
+        "balloonContentFooter": "<font size=1>Информация предоставлена: </font> <strong>этим балуном</strong>",
+        "clusterCaption": "<strong><s>Еще</s> одна</strong> метка",
+        "hintContent": "<strong>Текст  <s>подсказки</s></strong>"}}
